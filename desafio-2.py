726835f5d4aa3d0eec6ba08b19d0d48280fdedfb
Allan Kildare <allankildare27@gmail.com>
2021-01-22 04:43:01 -0300

    estudo de python iniciado

---
+++ desafio-2.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia que você nasceu? ')
+mes = input('E o mês? ')
+ano = input('E o ano? ')
+print('Você nasceu no dia ', dia,' de ', mes, ' de ', ano, '. Correto?') 
---
+++ desafio-3.py
@@ -0,0 +1,4 @@
+primeiro = int(input('Vamos fazer uma soma. Qual o primeiro número? '))
+segundo = int(input('Qual o segundo número? '))
+soma = primeiro + segundo
+print('O resultado desta soma é', soma)
